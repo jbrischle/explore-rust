250ecd2c24d1af27e87a9a26a4ccfc968d5dc792
janick-brischle <janick.brischle@de.markant.com>
2023-04-21 10:48:24 +0200

    finish example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::io::{BufWriter, stdout};
+
+use ferris_says::say;
+
 fn main() {
-    println!("Hello, world!");
+    let mut writer = BufWriter::new(stdout().lock());
+    say("hello world".as_bytes(), 5, &mut writer).unwrap()
 }
