e824eff86debbb7b5d088e573837d4427e95085e
janick-brischle <janick.brischle@de.markant.com>
2023-04-21 11:47:39 +0200

    implement guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,42 @@
-use std::io::{BufWriter, stdout};
+use std::cmp::Ordering;
+use std::io;
 
-use ferris_says::say;
+use rand::Rng;
 
 fn main() {
-    let mut writer = BufWriter::new(stdout().lock());
-    say("hello world", 5, &mut writer).unwrap()
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng()
+        .gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess
+            .trim()
+            .parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type in a number.");
+                continue;
+            }
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
